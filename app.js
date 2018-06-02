8b5680506856ce05a8b1bc900d562b3f4a6d9917
Laura Fernández González <laurafg@MacBook-Pro-de-Laura.local>
2018-06-02 21:23:22 +0200

    p7

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+
+
+//Aquí encontramos todos los elementos que va a generar el sistema
+//Se instalan todos los middlewares con los que va a funcionar
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,20 +12,23 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var SequelizeStore = require('connect-session-sequelize')(session.Store);
-var partials = require('express-partials');
+var partials = require('express-partials'); //es un MW para "sacar factor común", introduce layout.ejs 
+                                            //en layout habría que introducir las vistas comunes a todas las páginas
+                                            //viewport es para ajustar el tamaño de la pagina a los distintos dispositivos
 var flash = require('express-flash');
 var methodOverride = require('method-override');
 
-var index = require('./routes/index');
+var index = require('./routes/index'); //importa los enroutadores (ficheros en routes)
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //indica que las vistas se encuentran en el directorio views
+app.set('view engine', 'ejs'); //indican que trabajamos con las vistas de modelo ejs
+
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))); //public es el directorio y favicon.ico el fichero
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -55,6 +64,8 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', index);
+//MIDDLEWARES(los anteriores tambíen lo son, los que ponen app.use)
+app.use('/', index); //cuando accedemos a la url :localhost:puerto/ accedemos a index y se ejecuta lo que esté en este en función de lo que haya pasado
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
